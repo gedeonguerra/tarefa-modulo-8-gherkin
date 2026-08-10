8105d9bd1770827faf707d3b4d5a3570659d80b1
Gedeon Guerra <netoguerra360@gmail.com>
2026-08-10 00:40:03 -0300

    implementação de hooks e refatoração do LoginPage

--- e2e/pages/LoginPage.js
+++ e2e/pages/LoginPage.js
@@ -18,7 +18,9 @@ class LoginPage {
     return this.page.locator("[data-test='login-error'], [data-test='email-error'], [data-test='password-error']");
   }
   async logout() {
-    await this.page.click("[data-test='nav-menu']");
+    const navMenu = this.page.locator("[data-test='nav-menu']");
+    await navMenu.waitFor({ state: "visible", timeout: 30000 });
+    await navMenu.click();
     await this.page.click("[data-test='nav-sign-out']");
   }
 }
