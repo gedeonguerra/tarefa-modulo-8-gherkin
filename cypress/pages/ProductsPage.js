e88a6b1bd6226176f70087bffb94d9cadc3012bc
Gedeon Guerra <netoguerra360@gmail.com>
2026-08-12 19:13:46 -0300

    fix: aguardar resposta da API de produtos antes de interagir com a lista

--- cypress/pages/ProductsPage.js
+++ cypress/pages/ProductsPage.js
@@ -1,10 +1,14 @@
 class ProductsPage {
   visit() {
+    cy.intercept("GET", /\/products(\?.*)?$/).as("getProducts");
     cy.visit("/");
+    cy.wait("@getProducts", { timeout: 15000 });
   }
   search(term) {
+    cy.intercept("GET", /\/products\/search(\?.*)?$/).as("searchProducts");
     cy.get("[data-test='search-query']").clear().type(term);
     cy.get("[data-test='search-submit']").click();
+    cy.wait("@searchProducts", { timeout: 15000 });
   }
   productNames() {
     return cy.get("[data-test='product-name']");
